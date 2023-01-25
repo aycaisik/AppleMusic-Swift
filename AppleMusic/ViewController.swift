12c3bff3896f46c77d5c32b7f4adb8ebf873de78
Ayça Işık <117450558+aycaisik@users.noreply.github.com>
2023-01-25 23:44:14 +0300

    Added Assets and UIElements.

--- AppleMusic/ViewController.swift
+++ AppleMusic/ViewController.swift
@@ -8,10 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - UI Elements
+    @IBOutlet var albumImageView: UIImageView!
+    
+    @IBOutlet var reverseBackgroundView: UIView!
+    @IBOutlet var reverseButton: UIButton!
+    
+    @IBOutlet var playPauseBackgroundView: UIView!
+    @IBOutlet var playPauseButton: UIButton!
+    
+    @IBOutlet var forwardBackgroundView: UIView!
+    @IBOutlet var forwardButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
