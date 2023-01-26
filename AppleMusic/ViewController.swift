dca175392d363d5bf7a3fc881070d214adad8666
Ayça Işık <117450558+aycaisik@users.noreply.github.com>
2023-01-26 20:16:07 +0300

    Added LifeCycle.

--- AppleMusic/ViewController.swift
+++ AppleMusic/ViewController.swift
@@ -21,11 +21,51 @@ class ViewController: UIViewController {
     @IBOutlet var forwardBackgroundView: UIView!
     @IBOutlet var forwardButton: UIButton!
 
+    // MARK: - Properties
+    var isPlaying: Bool = true {
+        didSet {
+            if isPlaying {
+                playPauseButton.setImage(UIImage(named: "pause"), for: .normal)
+            }else {
+                playPauseButton.setImage(UIImage(named: "play"), for: .normal)
+            }
+        }
+    }
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        reverseBackgroundView.layer.cornerRadius = 35
+        reverseBackgroundView.clipsToBounds = true
+        reverseBackgroundView.alpha = 0
+        
+        playPauseBackgroundView.layer.cornerRadius = 35
+        playPauseBackgroundView.clipsToBounds = true
+        playPauseBackgroundView.alpha = 0
+        
+        forwardBackgroundView.layer.cornerRadius = 35
+        forwardBackgroundView.clipsToBounds = true
+        forwardBackgroundView.alpha = 0
     }
-
-
-}
-
+    
+    // MARK: - Actions
+    @IBAction func playPauseButtonTapped(_ button: UIButton) {
+        if isPlaying {
+            UIView.animate(withDuration: 0.5) {
+                self.albumImageView.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
+            }
+        }else {
+            UIView.animate(withDuration: 0.5) {
+                self.albumImageView.transform = .identity
+            }
+        }
+        
+        isPlaying.toggle()
+    }
+        
+        
+        
+    }
+    
+   
